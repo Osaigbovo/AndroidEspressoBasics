98ad409209a3ace49d99cf8c327b94864b8e060a
Osaigbovo <odiase.osaigbovo@gmail.com>
2019-09-17 22:17:48 +0100

    Write Espresso test for RecyclerView

--- app/src/androidTest/java/com/sqisland/android/espresso/hello/RecyclerActivityTest.kt
+++ app/src/androidTest/java/com/sqisland/android/espresso/hello/RecyclerActivityTest.kt
@@ -1,9 +1,17 @@
 package com.sqisland.android.espresso.hello
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.sqisland.android.espresso.recyclerview.RecyclerActivity
+import org.junit.Rule
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class RecyclerActivityTest {
+
+
+    @Rule
+    @JvmField
+    var activityRule = ActivityTestRule<RecyclerActivity>(RecyclerActivity::class.java)
 
 }
