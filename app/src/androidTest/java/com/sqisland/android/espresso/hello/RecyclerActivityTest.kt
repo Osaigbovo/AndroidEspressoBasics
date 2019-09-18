697575a6020e7c03a13801b5cf983ba08574f8bd
Osaigbovo <odiase.osaigbovo@gmail.com>
2019-09-19 00:23:22 +0100

    final commit

--- app/src/androidTest/java/com/sqisland/android/espresso/hello/RecyclerActivityTest.kt
+++ app/src/androidTest/java/com/sqisland/android/espresso/hello/RecyclerActivityTest.kt
@@ -1,9 +1,17 @@
 package com.sqisland.android.espresso.hello
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import com.sqisland.android.espresso.recyclerview.RecyclerActivity
+import com.sqisland.android.espresso.recyclerview.TextViewHolder
+import org.hamcrest.CoreMatchers.not
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -13,5 +21,19 @@ class RecyclerActivityTest {
     @Rule
     @JvmField
     var activityRule = ActivityTestRule<RecyclerActivity>(RecyclerActivity::class.java)
+
+    @Test
+    fun clickItem() {
+
+        onView(withId(R.id.footer))
+                .check(matches(not(isDisplayed())))
+
+        onView(withId(R.id.recycler_view))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<TextViewHolder>(27, click()))
+
+        onView(withId(R.id.footer))
+                .check(matches(withText("27")))
+                .check(matches(isDisplayed()))
+    }
 
 }
